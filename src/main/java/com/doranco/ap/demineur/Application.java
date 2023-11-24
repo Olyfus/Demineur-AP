1291db72e4f7ef21f017a8f39d27c8337084ff28
Olyfus <olyfus42300@gmail.com>
2023-11-24 14:05:02 +0100

    First init

---
+++ src/main/java/com/doranco/ap/demineur/Application.java
@@ -0,0 +1,13 @@
+package com.doranco.ap.demineur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
